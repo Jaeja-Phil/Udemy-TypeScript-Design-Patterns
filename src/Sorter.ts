61d32ee22dd2facf7c061134d98b6af146344902
Phillip Choi <jaeja-phil@gmail.com>
2020-08-19 20:11:17 +0900

    ADD linkedlist and good implementation of typescript

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,20 +1,21 @@
-import { NumbersCollection } from './NumbersCollection'
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 
-export class Sorter {
-  // collection: number[];
-
-  // constructor(collection: number[]) {
-  //   this.collection = collection;
-  // }
-  constructor(public collection: NumbersCollection /* TODO: FIX_ME */) { }
+export abstract class Sorter {
+  abstract compare(left: number, right: number): boolean;
+  abstract swap(left: number, right: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
