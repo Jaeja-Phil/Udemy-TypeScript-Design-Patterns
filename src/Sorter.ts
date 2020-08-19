5ba0bb49b9c1afc6be91a7ca94f2fd801ab1d80c
Phillip Choi <jaeja-phil@gmail.com>
2020-08-19 16:41:23 +0900

    First commit

---
+++ src/Sorter.ts
@@ -0,0 +1,22 @@
+import { NumbersCollection } from './NumbersCollection'
+
+export class Sorter {
+  // collection: number[];
+
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
+  constructor(public collection: NumbersCollection /* TODO: FIX_ME */) { }
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
+        }
+      }
+    }
+  }
+}
